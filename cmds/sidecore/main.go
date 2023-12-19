0178df8b19f7a3044df531b052fd974f00adb755
Ron Minnich <rminnich@gmail.com>
2023-12-19 09:15:10 -0800

    remove early test code
    Just to get it working at all, we started a server if -nfs was set.
    
    We don't need that any more.
    
    Signed-off-by: Ron Minnich <rminnich@gmail.com>

--- cmds/sidecore/main.go
+++ cmds/sidecore/main.go
@@ -342,11 +342,6 @@ func main() {
 	}
 	verbose("home is %q", home)
 	var wg sync.WaitGroup
-	if *srvnfs {
-		wg.Add(1)
-		err := srv()
-		log.Printf("nfs: %v", err)
-	}
 	// The remote system, for now, is always Linux or a standard Unix (or Plan 9)
 	// It will never be darwin (go argue with Apple)
 	// so /tmp is *always* /tmp
