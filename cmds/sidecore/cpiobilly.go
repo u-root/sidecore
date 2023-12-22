67b87b76aa3ca589de4389ce412284c6a9bbdd89
Ron Minnich <rminnich@gmail.com>
2023-12-21 16:51:37 -0800

    add our own auth handler prior to making it use the nonce.
    Signed-off-by: Ron Minnich <rminnich@gmail.com>

--- cmds/sidecore/cpiobilly.go
+++ cmds/sidecore/cpiobilly.go
@@ -15,10 +15,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"io/fs"
 	"log"
+	"net"
 	"os"
 	"path"
 	"path/filepath"
@@ -441,10 +443,58 @@ func srvNFS(cl *client.Cmd, n string) error {
 	}
 	verbose("listener %T %v addr %v port %v", l, l, l.Addr().String(), portnfs)
 
-	handler := nfshelper.NewNullAuthHandler(ROFS{mem})
+	handler := NewNullAuthHandler(ROFS{mem})
 	cacheHelper := nfshelper.NewCachingHandler(handler, 1024)
 	go func() {
 		fmt.Printf("%v", nfs.Serve(l, cacheHelper))
 	}()
 	return nil
+}
+
+// auth handler for our special sauce.
+
+// NewNullAuthHandler creates a handler for the provided filesystem
+func NewNullAuthHandler(fs billy.Filesystem) nfs.Handler {
+	return &NullAuthHandler{fs}
+}
+
+// NullAuthHandler returns a NFS backing that exposes a given file system in response to all mount requests.
+type NullAuthHandler struct {
+	fs billy.Filesystem
+}
+
+// Mount backs Mount RPC Requests, allowing for access control policies.
+func (h *NullAuthHandler) Mount(ctx context.Context, conn net.Conn, req nfs.MountRequest) (status nfs.MountStatus, hndl billy.Filesystem, auths []nfs.AuthFlavor) {
+	status = nfs.MountStatusOk
+	hndl = h.fs
+	auths = []nfs.AuthFlavor{nfs.AuthFlavorNull}
+	return
+}
+
+// Change provides an interface for updating file attributes.
+func (h *NullAuthHandler) Change(fs billy.Filesystem) billy.Change {
+	if c, ok := h.fs.(billy.Change); ok {
+		return c
+	}
+	return nil
+}
+
+// FSStat provides information about a filesystem.
+func (h *NullAuthHandler) FSStat(ctx context.Context, f billy.Filesystem, s *nfs.FSStat) error {
+	return nil
+}
+
+// ToHandle handled by CachingHandler
+func (h *NullAuthHandler) ToHandle(f billy.Filesystem, s []string) []byte {
+	return []byte{}
+}
+
+// FromHandle handled by CachingHandler
+func (h *NullAuthHandler) FromHandle([]byte) (billy.Filesystem, []string, error) {
+	return nil, []string{}, nil
+}
+
+// HandleLImit handled by cachingHandler
+func (h *NullAuthHandler) HandleLimit() int {
+	return -1
 }
